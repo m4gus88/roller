0d3703f0827089dc08ae1346fa34f93c9293d57d
Gergo Schmall <m4gus88@gmail.com>
2014-11-08 12:10:58 +0100

    Added arguments for //chat command.

--- src/main/java/com/verygery/roller/ChatCreatorParser.java
+++ src/main/java/com/verygery/roller/ChatCreatorParser.java
@@ -1,5 +1,8 @@
 package com.verygery.roller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.skype.Chat;
 import com.skype.ChatMessage;
 import com.skype.Skype;
@@ -15,20 +18,29 @@ public class ChatCreatorParser implements MessageParser {
   public boolean parse(ChatMessage message) throws SkypeException {
     boolean result = false;
     if (message.getContent().startsWith("//chat")) {
+      String[] args = message.getContent().split(" ");
       Chat chat = message.getChat();
       User[] members = chat.getAllMembers();
-      String[] userIds = new String[members.length];
-      String me = Skype.getProfile().getId();
-      for (int i = 0; i < members.length; i++) {
-        String member = members[i].getId();
-        if (!member.equals(me)) {
-          userIds[i] = member;
-        }
+      List<String> userIds = new ArrayList<>();
+      fetchChatUsers(members, userIds);
+      for (int i = 1; i < args.length; i++) {
+        userIds.add(args[i]);
       }
-      Skype.chat(userIds).send(message.getSenderDisplayName() + " created this chat with Roller.");
+      String[] userIdArray = userIds.toArray(new String[0]);
+      Skype.chat(userIdArray).send(message.getSenderDisplayName() + " created this chat with Roller.");
       chat.send(message.getSenderDisplayName() + " created a new chat.");
       result = true;
     }
     return result;
+  }
+
+  private void fetchChatUsers(User[] members, List<String> userIds) throws SkypeException {
+    String me = Skype.getProfile().getId();
+    for (int i = 0; i < members.length; i++) {
+      String member = members[i].getId();
+      if (!member.equals(me)) {
+        userIds.add(member);
+      }
+    }
   }
 }
